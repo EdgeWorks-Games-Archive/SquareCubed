31d29907009cb04b88c75d497d8f6b6aa3ae4b37
Layl Conway <laylconway@live.com>
2014-02-28 03:43:33 +0100

    Added pretty stars.

--- SQCore.Client/ClientPlugin.cs
+++ SQCore.Client/ClientPlugin.cs
@@ -1,13 +1,19 @@
-﻿using SQCore.Common;
+﻿using SQCore.Client.Stars;
+using SQCore.Common;
 using SquareCubed.Client;
 
 namespace SQCore.Client
 {
 	public class ClientPlugin : CommonPlugin, IClientPlugin
 	{
+		private StarsBackground _stars;
+
 		public ClientPlugin(SquareCubed.Client.Client client)
 		{
 			Logger.LogInfo("Initializing core plugin...");
+
+			_stars = new StarsBackground(client);
+
 			Logger.LogInfo("Finished initializing core plugin!");
 		}
 	}
---
+++ SQCore.Client/Stars/StarsBackground.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace SQCore.Client.Stars
+{
+	internal class StarsBackground
+	{
+		private readonly List<StarData> _starData = new List<StarData>();
+		private readonly Random _random = new Random();
+		private readonly Vector2 _resolution;
+
+		public StarsBackground(SquareCubed.Client.Client client)
+		{
+			_resolution = new Vector2(client.Window.Width, client.Window.Height);
+
+			// Generate star data
+			for (var i = 0; i < 200; i++)
+			{
+				// Generate a new star
+				_starData.Add(new StarData
+				{
+					Position = new Vector2(
+						(float)_random.NextDouble() * _resolution.X,
+						(float)_random.NextDouble() * _resolution.Y),
+					Rotation = (float)_random.NextDouble() * 360.0f,
+					Scale = (float)_random.NextDouble() * 1.0f + 0.5f
+				});
+			}
+
+			// Bind rendering event
+			client.BackgroundRenderTick += Render;
+		}
+
+		private void Render(object sender, float e)
+		{
+			// Temporarily set the camera to something more convenient
+			GL.MatrixMode(MatrixMode.Projection);
+			GL.PushMatrix();
+			GL.LoadIdentity();
+			GL.Ortho(
+				0, _resolution.X,
+				0, _resolution.Y,
+				0.0, 4.0);
+
+			// Draw all the stars
+			GL.MatrixMode(MatrixMode.Modelview);
+			foreach (var star in _starData)
+			{
+				GL.PushMatrix();
+
+				// Move and rotate to the correct position
+				GL.Translate(star.Position.X, star.Position.Y, 0);
+				GL.Rotate(star.Rotation, 0.0f, 0.0f, 1.0f);
+				GL.Scale(star.Scale, star.Scale, star.Scale);
+
+				// Draw a white square
+				GL.Begin(PrimitiveType.Quads);
+				GL.Color3(Color.White);
+
+				GL.Vertex2(-1, -1); // Left Bottom
+				GL.Vertex2(1, -1); // Right Bottom
+				GL.Vertex2(1, 1); // Right Top
+				GL.Vertex2(-1, 1); // Left Top
+
+				GL.End();
+
+				GL.PopMatrix();
+			}
+
+			// Reset the resolution
+			GL.MatrixMode(MatrixMode.Projection);
+			GL.PopMatrix();
+		}
+
+		private class StarData
+		{
+			public Vector2 Position { get; set; }
+			public float Rotation { get; set; }
+			public float Scale { get; set; }
+		}
+	}
+}
